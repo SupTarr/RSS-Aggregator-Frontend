3913ce328ed0bf316dc9f29221cece167203f12c
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-12-18 17:50:32 +0700

    feat: add loading components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement, Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Loading, Size } from "./components/Loading.tsx";
 import "./App.css";
 
 const PageLogin = lazy(() => import("./pages/PageLogin.tsx"));
@@ -7,7 +8,7 @@ const PageLogin = lazy(() => import("./pages/PageLogin.tsx"));
 const App: React.FC = (): ReactElement => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<Loading size={Size.Large} />}>
         <Routes>
           <Route path="/" Component={PageLogin} />
         </Routes>
---
+++ src/components/Loading.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from "react";
+
+enum Size {
+  ExSmall,
+  Small,
+  Medium,
+  Large,
+}
+
+const Loading: React.FC<{ size: Size }> = ({ size }): ReactElement => {
+  let classModifier = "xs";
+  switch (size) {
+    case Size.Small:
+      classModifier = "sm";
+      break;
+    case Size.Medium:
+      classModifier = "md";
+      break;
+    case Size.Large:
+      classModifier = "lg";
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <span className={`loading loading-spinner loading-${classModifier}`}></span>
+  );
+};
+
+export { Loading, Size };
